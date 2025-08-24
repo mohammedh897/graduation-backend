5c5941539e128147a18d35bcf6f4b28a4c945daa
Mohammed Hussein <mohamedh246810@gmail.com>
2025-08-24 11:52:04 +0300

    description optional

--- models/Task.js
+++ models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, required: false },
 
 
     // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
